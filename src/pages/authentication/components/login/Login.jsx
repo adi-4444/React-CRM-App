6081388c340a7c60130897d5b9d352ac8c52ce2e
Adi <adinarayana4444@gmail.com>
2022-10-19 23:52:54 +0530

    React CRM APP

--- src/pages/authentication/components/login/Login.jsx
+++ src/pages/authentication/components/login/Login.jsx
@@ -3,25 +3,25 @@ import './Login.css'
 
 
 const Login = ({setAuth}) => {
-  const [email, setEmail] = useState('');
+  const [userid, setUserId] = useState('');
   const [password, setPassword] = useState('');
 
   const loginHandler = (e) => {
     e.preventDefault()
-    console.log({email,password})
+    console.log({userid,password})
   }
 
   return (
     <div className='login-body'>
         <h1>Welcome</h1>
       <div className="login-wrapper">
-
+ 
         <div className="login-form-group">
           <h2>Login</h2>
-
+        <form onSubmit={loginHandler}>
           <div className="form-group">
-            <input type="email" className='form-control' name='email *' placeholder="Email" autoFocus required
-            value={email} onChange={(e)=> setEmail(e.target.value)}
+            <input type="userid" className='form-control' name='userid *' placeholder="userid" autoFocus required
+            value={userid} onChange={(e)=> setUserId(e.target.value)}
             />
             <label className='form-label'>Email *</label>
           </div>
@@ -34,10 +34,10 @@ const Login = ({setAuth}) => {
           </div>
   
           <div  className="login-btn">
-            <button type='submit' onClick={loginHandler}>Login</button>
+            <input type='submit' value='Login'/>
             <p> Don't have an account ? <a href='#/' onClick={() => setAuth("register")}>Signup</a></p> 
           </div>
-
+        </form>
         </div>
 
       </div>
--- src/pages/authentication/components/signup/Signup.jsx
+++ src/pages/authentication/components/signup/Signup.jsx
@@ -1,25 +1,60 @@
 import React,{ useState } from 'react'
 import './Signup.css'
-
+import {resisterUser} from '../../apis/authApi'
 
 const Signup = ({setAuth}) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userType, SetUserType] = useState('');
   const [password, setPassword] = useState('');
   const [confirmpassword, setConfirmPassword] = useState('');
 
   const signupHandler = (e) => {
     e.preventDefault()
-    console.log({name:name,email:email,password:password})
+    const data = {name,email,userId,userType,password}
+    console.log(data)
+
+    // api call to Signup a new user
+    try {
+      resisterUser(data)
+      .then(res => {
+        console.log(res)
+        const {status,data} = res;
+        if(status === 201) {
+          const {name,email,userId,userType} = data;
+          localStorage.setItem("name", name);
+          localStorage.setItem("email", email);
+          localStorage.setItem("userId", userId);
+          localStorage.setItem("userType", userType);
+          // if success, i will redirect the user to login page
+          setAuth("login")
+        }
+      })
+      .catch(err => {
+        const errMsg = err?.response?.data?.message || err?.message;
+        console.log(errMsg)
+      })
+    } catch (err) {
+      const errMsg = err?.response?.data?.message || err?.message;
+      console.log(errMsg)
+    }
+    
+
+
+    // if failure, i will show an error
+
+
   }
 
   return (
     <div className='signup-body'>
-        <h1>Welcome</h1>
       <div className="signup-wrapper">
 
         <div className="signup-form-group">
           <h2>Signup</h2>
+
+          <form onSubmit={signupHandler}>
 
             <div className="form-group">
               <input type="text" className='form-control' name='name' placeholder="Name" autoFocus required
@@ -27,17 +62,31 @@ const Signup = ({setAuth}) => {
               <label className='form-label'>Name *</label>
             </div>
 
-          <div className="form-group">
-            <input type="text" className='form-control' name='email *' placeholder="Email" required
-            value={email} onChange={(e)=> setEmail(e.target.value)}/>
-            <label className='form-label'>Email *</label>
-          </div>
+            <div className="form-group">
+              <input type="text" className='form-control' name='email *' placeholder="Email" required
+              value={email} onChange={(e)=> setEmail(e.target.value)}/>
+              <label className='form-label'>Email *</label>
+            </div>
 
-          <div className="form-group">
-            <input type="password" className='form-control' name='password' placeholder="Password" required
-            value={password} onChange={(e)=> setPassword(e.target.value)}/>
-            <label className='form-label'>Password *</label>
-          </div>
+            <div className="form-group">
+              <input type="text" className='form-control' name='userId *' placeholder="userId" required
+              value={userId} onChange={(e)=> setUserId(e.target.value)}/>
+              <label className='form-label'>User ID *</label>
+            </div>
+
+            <div className='user-type'>
+              <label>Select User Type *  </label>
+                <select value={userType} onChange={(e) => SetUserType(e.target.value)} required>
+                    <option value='ENGINEER'>Engineer</option>
+                    <option value='CUSTOMER'>Customer</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+              <input type="password" className='form-control' name='password' placeholder="Password" required
+              value={password} onChange={(e)=> setPassword(e.target.value)}/>
+              <label className='form-label'>Password *</label>
+            </div>
 
             <div className="form-group">
              <input type="password" className='form-control' name='confirmpassword' placeholder="ConfirmPassword" required
@@ -52,10 +101,12 @@ const Signup = ({setAuth}) => {
               }
             </div>
 
-          <div  className="signup-btn">
-            <button type='submit' onSubmit={signupHandler}>Signup</button>
-            <p>Already have an account ? <a href='#/' onClick={() => setAuth("login")}>Login</a></p>
-          </div>
+            <div  className="signup-btn">
+              <input type='submit' value='Signup' className='submit-btn'/>
+              <p>Already have an account ? <a href='#/' onClick={() => setAuth("login")}>Login</a></p>
+            </div>
+
+          </form>
 
         </div>
 
