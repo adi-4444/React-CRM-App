97c03a4518ed1e38f39a50d07c2613f8c34067c1
Adi Narayana Reddy <adinarayana4444@gmail.com>
2022-11-15 21:39:10 +0530

    React CRM App

--- src/pages/admin/components/content/components/users/Users.jsx
+++ src/pages/admin/components/content/components/users/Users.jsx
@@ -3,11 +3,12 @@ import "./Users.css";
 import UserTable from "./userTable/UserTable";
 import { fetchUsers } from "../../../../apis/users";
 import UserModel from "./userModel/UserModel";
-
+import { updateUser } from "../../../../apis/users";
 const Users = () => {
 	const [usersData, setUsersData] = useState([]);
 	const [selectedUserDetails, setSelectedUserDetails] = useState({});
 	const [userModel, setUserModel] = useState(false);
+	const [userModelError, setUserModelError] = useState("");
 	useEffect(() => {
 		getUsers();
 	}, []);
@@ -29,9 +30,48 @@ const Users = () => {
 	};
 	const hiseUserModel = () => {
 		setUserModel(false);
+		setSelectedUserDetails({});
 	};
 	const showUserModel = () => {
 		setUserModel(true);
+	};
+	const selectedUserChange = (e) => {
+		const currentData = { ...selectedUserDetails };
+		if (e.target.name === "name") {
+			currentData.name = e.target.value;
+		} else if (e.target.name === "email") {
+			currentData.email = e.target.value;
+		} else if (e.target.name === "userType") {
+			currentData.userTypes = e.target.value;
+		} else {
+			currentData.userStatus = e.target.value;
+		}
+		setSelectedUserDetails(currentData);
+	};
+	const userUpdate = (e) => {
+		e.preventDefault();
+		const data = {
+			userType: selectedUserDetails.userTypes,
+			userStatus: selectedUserDetails.userStatus,
+			userName: selectedUserDetails.name,
+		};
+		try {
+			updateUser(data, selectedUserDetails.userId)
+				.then((res) => {
+					const { status } = res;
+					if (status === 200) {
+						hiseUserModel();
+						getUsers();
+					}
+				})
+				.catch((err) => {
+					console.log(err.message);
+					setUserModelError(err.message);
+				});
+		} catch (err) {
+			console.log(err.message);
+			setUserModelError(err.message);
+		}
 	};
 	return (
 		<div>
@@ -41,11 +81,16 @@ const Users = () => {
 				setSelectedUserDetails={setSelectedUserDetails}
 				showUserModel={showUserModel}
 			/>
-			<UserModel
-				userModel={userModel}
-				hiseUserModel={hiseUserModel}
-				selectedUserDetails={selectedUserDetails}
-			/>
+			{userModel && (
+				<UserModel
+					userModel={userModel}
+					hiseUserModel={hiseUserModel}
+					selectedUserDetails={selectedUserDetails}
+					selectedUserChange={selectedUserChange}
+					userUpdate={userUpdate}
+					userModelError={userModelError}
+				/>
+			)}
 		</div>
 	);
 };
--- src/pages/admin/components/content/components/users/userModel/UserModel.jsx
+++ src/pages/admin/components/content/components/users/userModel/UserModel.jsx
@@ -6,11 +6,21 @@ import {
 	CModalHeader,
 	CModalTitle,
 	CModalFooter,
+	CButton,
 } from "@coreui/react";
-import { CButton } from "@coreui/react";
+import { USER_TYPES } from "../../../../../../../commom/constants/userTypes";
+
 const UserModel = (props) => {
-	const { userModel, hiseUserModel, selectedUserDetails } = props;
-	console.log(userModel);
+	const {
+		userModel,
+		hiseUserModel,
+		selectedUserDetails,
+		selectedUserChange,
+		userUpdate,
+		userModelError,
+	} = props;
+	const { name, email, userId, userTypes, userStatus } = selectedUserDetails;
+	console.log(name);
 	return (
 		<div>
 			<div className='modal-wrapper'>
@@ -23,13 +33,106 @@ const UserModel = (props) => {
 					<CModalHeader>
 						<CModalTitle>Update User</CModalTitle>
 					</CModalHeader>
-					<CModalBody>React Modal body text goes here.</CModalBody>
-					<CModalFooter>
-						<CButton color='secondary' onClick={hiseUserModel}>
-							Close
-						</CButton>
-						<CButton color='info'>Save changes</CButton>
-					</CModalFooter>
+					<form onSubmit={userUpdate}>
+						<CModalBody>
+							<div className='form-container my-3 mx-2'>
+								<label htmlFor='name' className='d-flex'>
+									<span>Name: </span>
+									<input
+										type='text'
+										id='name'
+										name='name'
+										className='form-control mx-2'
+										value={name}
+										onChange={selectedUserChange}
+									/>
+								</label>
+							</div>
+							<div className='form-container my-3 mx-2'>
+								<label htmlFor='email' className='d-flex'>
+									<span>Email: </span>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										className='form-control mx-2'
+										value={email}
+										onChange={selectedUserChange}
+										disabled
+									/>
+								</label>
+							</div>
+							<div className='form-container my-3 mx-2'>
+								<label htmlFor='userId' className='d-flex'>
+									<span>UserId: </span>
+									<input
+										type='text'
+										id='userId'
+										name='userId'
+										className='form-control mx-2'
+										value={userId}
+										onChange={selectedUserChange}
+										disabled
+									/>
+								</label>
+							</div>
+							<div className='form-container my-3 mx-2'>
+								<label htmlFor='userType' className='d-flex'>
+									<span>UserType: </span>
+									<select
+										name='userType'
+										id='userType'
+										className='form-select mx-2'
+										value={userTypes}
+										onChange={selectedUserChange}
+									>
+										<option value={USER_TYPES.ADMIN}>
+											{USER_TYPES.ADMIN}
+										</option>
+										<option value={USER_TYPES.ENGINEER}>
+											{USER_TYPES.ENGINEER}
+										</option>
+										<option value={USER_TYPES.CUSTOMER}>
+											{USER_TYPES.CUSTOMER}
+										</option>
+									</select>
+								</label>
+							</div>
+							<div className='form-container my-3 mx-2'>
+								<label htmlFor='userStatus' className='d-flex'>
+									<span>Status: </span>
+									<select
+										className='form-select mx-2'
+										name='userStatus'
+										id='userStatus'
+										value={userStatus}
+										onChange={selectedUserChange}
+									>
+										<option value='PENDING'>PENDING</option>
+										<option value='APPROVED'>
+											APPROVED
+										</option>
+										<option value='REJECTED'>
+											REJECTED
+										</option>
+									</select>
+								</label>
+							</div>
+							{userModelError && (
+								<div className='my-2 text-danger mx-2'>
+									{userModelError}
+								</div>
+							)}
+						</CModalBody>
+						<CModalFooter>
+							<CButton color='secondary' onClick={hiseUserModel}>
+								Close
+							</CButton>
+							<CButton type='submit' color='info'>
+								Save changes
+							</CButton>
+						</CModalFooter>
+					</form>
 				</CModal>
 			</div>
 		</div>
